d2b5764041585957bffc504bbc13162cfa4f4a98
Benjamin Watto <wattob@aldenv130.allegheny.edu>
2018-10-11 11:10:59 -0400

    added question 5

--- src/Lab4Program.r
+++ src/Lab4Program.r
@@ -26,7 +26,8 @@ dat_caliFocus$yearBlock[dat_caliFocus$year >= 1980] <- "NA"
 dat_caliFocus <- filter(dat_caliFocus, yearBlock != "NA")
 ggplot(data = dat_caliFocus ) + geom_bar(mapping = aes(x = state, y = count, fill = yearBlock), position = "dodge", stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=-0.01))
 ggplot(data = dat_caliFocus ) + geom_bar(mapping = aes(x = state, y = sqrt(count), fill = yearBlock), position = "dodge", stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=-0.01))
-Which seems to have more similar variability across years?
+# The 1950's seems to have more similar variability across years. I think this is because the data collected is much older and tends to be less reliable.
+
 
 #Question 4.
 dat_Focus <- filter(us_contagious_diseases)
@@ -38,6 +39,14 @@ dat_Focus <- filter(dat_Focus, yearBlock != "NA")
 ggplot(data = dat_Focus ) + geom_bar(mapping = aes(x = state, y = count, fill = yearBlock), position = "dodge", stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=-0.01))
 
 #Question 5.
+dat_Focus <- filter(us_contagious_diseases)
+dat_Focus$yearBlock[dat_Focus$year >= 1950] <- "1950’s"
+dat_Focus$yearBlock[dat_Focus$year >= 1960] <- "1960’s"
+dat_Focus$yearBlock[dat_Focus$year >= 1970] <- "1970’s"
+dat_Focus$yearBlock[dat_Focus$year >= 1980] <- "NA"
+dat_Focus <- filter(dat_Focus, yearBlock != "NA")
+dat_Focus <- mutate(dat_Focus, per100000rate = ((count*100000)/population)* (weeks_reporting/52))
+ggplot(data = dat_Focus ) + geom_bar(mapping = aes(x = state, y = count, fill = yearBlock), position = "dodge", stat = "identity") + geom_tile(mapping = aes(x = state, y = count, color = per100000rate)) + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=-0.01))
 
 #Question 6.
 
